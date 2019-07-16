1617594a81fb035f2559b52b9e6e743fe57a96b1
Tricodin <Tricodin@gmail.com>
2019-07-16 04:01:16 -0230

    Better searching, checks more jobs, and saves data as csv

--- JobSkillComparator.py
+++ JobSkillComparator.py
@@ -1,47 +1,90 @@
 from Skill_Info import Skill
 import xlrd
+import os
+
 
 for ExcelFileName in ['Knowledge.xlsx', 'Skills.xlsx', 'Abilities.xlsx']:
+
+    filename = ExcelFileName[:-5] + "Data.csv"
+    if os.path.exists(filename):
+        os.remove(filename)
+    f = open(filename, "w+")
+
     workbook = xlrd.open_workbook(ExcelFileName)
     worksheet = workbook.sheet_by_name(ExcelFileName[:-5])
 
-    firstJob = "Civil Engineers" #raw_input("Enter first job:")
+    firstJob = "Agricultural Engineers" #raw_input("Enter first job:")
     secondJob = "Marine Engineers" #raw_input("Enter second job:")
 
     skills = []
 
-    count = 1
-    for i in range(0, worksheet.nrows):
-        if worksheet.cell_value(i, 1) == firstJob and worksheet.cell_value(i, 4) == 'IM':
-            skillToAdd = worksheet.cell_value(i, 3)
-            added = False
-            for temp in skills:
-                if temp.skill == skillToAdd:
-                    added = True
-
-            if added == False:
-                s = Skill(skillToAdd)
-                s.setAgri(worksheet.cell_value(i, 6))
-                skills.append(s)
 
     for i in range(0, worksheet.nrows):
-        if worksheet.cell_value(i, 1) == secondJob and worksheet.cell_value(i, 4) == 'IM':
+        if worksheet.cell_value(i, 4) == 'IM':
             skillToAdd = worksheet.cell_value(i, 3)
             added = False
-            for temp in skills:
-                if temp.skill == skillToAdd:
-                    added = True
-                    temp.setNaval(worksheet.cell_value(i, 6))
 
-            if added == False:
-                s = Skill(skillToAdd)
-                s.setNaval(worksheet.cell_value(i, 6))
-                skills.append(s)
+            if worksheet.cell_value(i, 1) == "Marine Engineers":
+                for temp in skills:
+                    if temp.skill == skillToAdd:
+                        added = True
+                        temp.setNaval(worksheet.cell_value(i, 6))
+                if added == False:
+                    s = Skill(skillToAdd)
+                    s.setNaval(worksheet.cell_value(i, 6))
+                    skills.append(s)
 
+            elif worksheet.cell_value(i, 1) == "Agricultural Engineers":
+                for temp in skills:
+                    if temp.skill == skillToAdd:
+                        added = True
+                        temp.setAgri(worksheet.cell_value(i, 6))
+                if added == False:
+                    s = Skill(skillToAdd)
+                    s.setAgri(worksheet.cell_value(i, 6))
+                    skills.append(s)
+
+            if worksheet.cell_value(i, 1) == "Electrical Engineers":
+                for temp in skills:
+                    if temp.skill == skillToAdd:
+                        added = True
+                        temp.setElec(worksheet.cell_value(i, 6))
+                if added == False:
+                    s = Skill(skillToAdd)
+                    s.setElec(worksheet.cell_value(i, 6))
+                    skills.append(s)
+
+            if worksheet.cell_value(i, 1) == "Civil Engineers":
+                for temp in skills:
+                    if temp.skill == skillToAdd:
+                        added = True
+                        temp.setCivil(worksheet.cell_value(i, 6))
+                if added == False:
+                    s = Skill(skillToAdd)
+                    s.setCivil(worksheet.cell_value(i, 6))
+                    skills.append(s)
+
+            if worksheet.cell_value(i, 1) == "Mechanical Engineers":
+                for temp in skills:
+                    if temp.skill == skillToAdd:
+                        added = True
+                        temp.setMech(worksheet.cell_value(i, 6))
+                if added == False:
+                    s = Skill(skillToAdd)
+                    s.setMech(worksheet.cell_value(i, 6))
+                    skills.append(s)
+
+
+    #skills.sort()
+    #print ExcelFileName[:-5] + " Level:"
+    #print ""
     skills.sort()
-    print ExcelFileName[:-5] + " Level:"
-    print ""
+    f.write("Skill Name,Naval,Agricultural,Civil,Electrical,Mechanical\n")
     for s in skills:
-        s.get()
+        f.write(s.makeCSV() + '\n')
+        #s.get()
     skills = []
-    print ""
+    #print ""
+
+
+    f.close()
--- Skill_Info.py
+++ Skill_Info.py
@@ -3,10 +3,13 @@ class Skill:
         self.skill = skill
         self.naval_data = -1
         self.agri_data = -1
+        self.civil_data = -1
+        self.elec_data = -1
+        self.mech_data = -1
         self.diff = 0
 
     def __lt__(self, other):
-        return self.diff < other.diff
+        return self.naval_data < other.naval_data
 
     def getDiff(self):
         if self.naval_data != -1 and self.agri_data != -1:
@@ -16,11 +19,24 @@ class Skill:
 
     def setNaval(self, data):
         self.naval_data = data
-        self.getDiff()
+        #self.getDiff()
         
     def setAgri(self, data):
         self.agri_data = data
-        self.getDiff()
+        #self.getDiff()
+
+    def setCivil(self, data):
+        self.civil_data = data
+
+    def setElec(self, data):
+        self.elec_data = data
+
+    def setMech(self, data):
+        self.mech_data = data
+
 
     def get(self):
-        print "Skill: " + self.skill.ljust(29, ' ') + "\tNaval:"  + str(self.naval_data).ljust(6, ' ') + "\tCivil:"  + str(self.agri_data).ljust(6, ' ') + "\tDifference:" + str(self.diff)
+        print "Skill: " + self.skill.ljust(33, ' ') + "\tNaval:"  + str(self.naval_data).ljust(6, ' ') + "\tAgricultural:"  + str(self.agri_data).ljust(6, ' ') + "\tDifference:" + str(self.diff)
+
+    def makeCSV(self):
+        return self.skill + "," + str(self.naval_data) + "," + str(self.agri_data) + "," + str(self.civil_data) + "," + str(self.elec_data) + "," + str(self.mech_data)
