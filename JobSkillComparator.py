4fddfabc01de06270d1e3a93b2084229fa0cf5b6
Tricodin <Tricodin@gmail.com>
2019-07-29 22:59:32 -0230

    create new branch to solve system of equations

--- JobSkillComparator.py
+++ JobSkillComparator.py
@@ -1,9 +1,10 @@
 from Skill_Info import Skill
 import xlrd
 import os
+import numpy as np
 
-
-for ExcelFileName in ['Knowledge.xlsx', 'Skills.xlsx', 'Abilities.xlsx']:
+#'Knowledge.xlsx', 'Skills.xlsx', 'Abilities.xlsx'
+for ExcelFileName in ['Skills.xlsx']:
 
     filename = ExcelFileName[:-5] + "Data.csv"
     if os.path.exists(filename):
@@ -15,65 +16,49 @@ for ExcelFileName in ['Knowledge.xlsx', 'Skills.xlsx', 'Abilities.xlsx']:
 
     firstJob = "Agricultural Engineers" #raw_input("Enter first job:")
     secondJob = "Marine Engineers" #raw_input("Enter second job:")
+    jobList = ["Marine Engineers", "Agricultural Engineers", "Electrical Engineers", "Civil Engineers", "Mechanical Engineers",
+               "Anthropologists", "Archeologists", "Software Developers, Applications", "Lawyers", "Mathematicians",
+               "Nuclear Engineers", "Computer Hardware Engineers", "Geographers", "Economists", "Historians",
+               "Computer Programmers", "Chemical Technicians", "Geoscientists, Except Hydrologists and Geographers", "Librarians", "Dental Hygienists",
+               "Pharmacy Aides", "Computer Operators", "Sailors and Marine Oilers", "Microbiologists", "Mental Health Counselors",
+               "Registered Nurses", "Pharmacists", "Aerospace Engineers", "Materials Scientists", "Database Administrators",
+               "Locomotive Engineers", "Interviewers, Except Eligibility and Loan", "Accountants", "Financial Analysts", "Graphic Designers"]
+
+    computerizationProbability = np.array([0.01, 0.49, 0.1, 0.019, 0.011,
+                                  0.0077, 0.0077, 0.042, 0.035, 0.047,
+                                  0.07, 0.22, 0.25, 0.43, 0.44,
+                                  0.48, 0.57, 0.63, 0.65, 0.68,
+                                  0.72, 0.78, 0.83, 0.012, 0.0048,
+                                  0.009, 0.012, 0.017, 0.021, 0.03,
+                                  0.96, 0.94, 0.94, 0.23, 0.082])
 
     skills = []
 
+    a = np.empty((0,35), int)
 
-    for i in range(0, worksheet.nrows):
-        if worksheet.cell_value(i, 4) == 'IM':
-            skillToAdd = worksheet.cell_value(i, 3)
-            added = False
+    skilldata = []
+    test = [2, 2, 3]
 
-            if worksheet.cell_value(i, 1) == "Marine Engineers":
-                for temp in skills:
-                    if temp.skill == skillToAdd:
-                        added = True
-                        temp.setNaval(worksheet.cell_value(i, 6))
-                if added == False:
-                    s = Skill(skillToAdd)
-                    s.setNaval(worksheet.cell_value(i, 6))
-                    skills.append(s)
 
-            elif worksheet.cell_value(i, 1) == "Agricultural Engineers":
-                for temp in skills:
-                    if temp.skill == skillToAdd:
-                        added = True
-                        temp.setAgri(worksheet.cell_value(i, 6))
-                if added == False:
-                    s = Skill(skillToAdd)
-                    s.setAgri(worksheet.cell_value(i, 6))
-                    skills.append(s)
+    for j in range(0, len(jobList)):
 
-            if worksheet.cell_value(i, 1) == "Electrical Engineers":
-                for temp in skills:
-                    if temp.skill == skillToAdd:
-                        added = True
-                        temp.setElec(worksheet.cell_value(i, 6))
-                if added == False:
-                    s = Skill(skillToAdd)
-                    s.setElec(worksheet.cell_value(i, 6))
-                    skills.append(s)
+        skilldata = []
 
-            if worksheet.cell_value(i, 1) == "Civil Engineers":
-                for temp in skills:
-                    if temp.skill == skillToAdd:
-                        added = True
-                        temp.setCivil(worksheet.cell_value(i, 6))
-                if added == False:
-                    s = Skill(skillToAdd)
-                    s.setCivil(worksheet.cell_value(i, 6))
-                    skills.append(s)
+        for i in range(0, worksheet.nrows):
+            if worksheet.cell_value(i, 1) == jobList[j]:
+                skillToAdd = worksheet.cell_value(i, 3)
+                added = False
 
-            if worksheet.cell_value(i, 1) == "Mechanical Engineers":
-                for temp in skills:
-                    if temp.skill == skillToAdd:
-                        added = True
-                        temp.setMech(worksheet.cell_value(i, 6))
-                if added == False:
-                    s = Skill(skillToAdd)
-                    s.setMech(worksheet.cell_value(i, 6))
-                    skills.append(s)
+                if worksheet.cell_value(i, 4) == 'IM':
+                    skilldata.append(worksheet.cell_value(i, 6))
 
+        #print skilldata
+        #print len(skilldata)
+        a = np.append(a, [skilldata], axis = 0)
+
+    x = np.linalg.solve(a, computerizationProbability)
+    print x
+    exit()
 
     #skills.sort()
     #print ExcelFileName[:-5] + " Level:"
